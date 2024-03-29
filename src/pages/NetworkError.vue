f10d59a35d8196a46393fb0127520ffb8e327157
Ywoosang <opellong13@gmail.com>
2024-03-29 16:36:55 +0900

    feat: 사용자에게 보여지는 에러페이지 추가

--- src/pages/NetworkError.vue
+++ src/pages/NetworkError.vue
@@ -4,7 +4,8 @@
       <router-link :to="'/'"> <b>윤우상</b>블로그 </router-link>
     </div>
     <div class="content">
-      <h1>이런! 요청하신 페이지를 찾을 수 없네요 :(</h1>
+      <h1>이런! 네트워크 연결이 일시적으로 원활하지 않네요 :(</h1>
+      <p class="text">잠시 후 다시 시도해 주세요</p>
       <p class="text">문제가 지속된다면 아래 메일로 연락해주세요</p>
       <p class="mail">opellong13@gmail.com</p>
     </div>
