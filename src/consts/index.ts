200ecaa86582aa3512be916ea8cff9bbf49d4904
Ywoosang <opellong13@gmail.com>
2024-04-18 04:12:15 +0900

    feat: 파일 URL 과 에러 모듈 최대 개수에 해당하는 const 변수 추가

--- src/consts/index.ts
+++ src/consts/index.ts
@@ -1,6 +1,8 @@
 export const BASE_URL =
   process.env.VUE_APP_BACKEND_DOMAIN || "http://localhost:3000";
-
+export const FILE_BASE_URL =
+  process.env.VUE_APP_FILE_BASE_URL || "http://localhost:3000";
 export const GOOGLE_LOGIN_URL = `${BASE_URL}/auth/google`;
 export const GITHUB_LOGIN_URL = `${BASE_URL}/auth/github`;
 export const KAKAO_LOGIN_URL = `${BASE_URL}/auth/kakao`;
+export const MAX_MODAL_COUNT = 5;
