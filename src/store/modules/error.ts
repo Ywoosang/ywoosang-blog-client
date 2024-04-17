153549039e7393a20ce3a82fe7afe7e95bca3a78
Ywoosang <opellong13@gmail.com>
2024-04-18 04:32:54 +0900

    feat: 사용자에게 표시되는 에러 모달이 지정한 최대 개수를 넘어가지 않도록 조정

--- src/store/modules/error.ts
+++ src/store/modules/error.ts
@@ -1,35 +1,37 @@
-import { Module, MutationTree, GetterTree } from 'vuex';
-import { RootState, ErrorState } from '@/types/interfaces';
+import { Module, MutationTree, GetterTree } from "vuex";
+import { RootState, ErrorState } from "@/types/interfaces";
 import { v4 as uuidv4 } from "uuid"; // ES Modules
-
-const maxCount = 5;
+import { MAX_MODAL_COUNT } from "@/consts";
 
 const state: ErrorState = {
-    modalList: []
+  modalList: [],
 };
 
 const mutations: MutationTree<ErrorState> = {
-    ADD_MODAL(state, content: string) {
-		const key = uuidv4();
-        state.modalList.push({
-			key,
-			content
-		});
-    },
-	REMOVE_MODAL(state, key) {
-		state.modalList = state.modalList.filter((modal) => modal.key != key);
-	}
+  ADD_MODAL(state, content: string) {
+    const key = uuidv4();
+    if (state.modalList.length == MAX_MODAL_COUNT) {
+      state.modalList.shift();
+    }
+    state.modalList.push({
+      key,
+      content,
+    });
+  },
+  REMOVE_MODAL(state, key) {
+    state.modalList = state.modalList.filter((modal) => modal.key != key);
+  },
 };
 
 const getters: GetterTree<ErrorState, RootState> = {
-    getModalList: (state) => state.modalList
+  getModalList: (state) => state.modalList,
 };
- 
+
 const errorModule: Module<ErrorState, RootState> = {
-	namespaced: true,
-	state,
-	mutations,
-	getters,
+  namespaced: true,
+  state,
+  mutations,
+  getters,
 };
 
 export default errorModule;
