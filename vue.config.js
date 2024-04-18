47110976975acd3ea6aec717e166d98d684890ca
Ywoosang <opellong13@gmail.com>
2024-04-19 03:13:29 +0900

    chore: 불필요한 주석 제거

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,16 @@
-const { defineConfig } = require('@vue/cli-service');
-const webpack = require('webpack');
+const { defineConfig } = require('@vue/cli-service')
+const webpack = require('webpack')
 
 module.exports = defineConfig({
-	configureWebpack: {
-		plugins: [
-			new webpack.DefinePlugin({
-				__VUE_PROD_HYDRATION_MISMATCH_DETAILS__: true,
-			}),
-		],
-	},
-	devServer: {
-		// 구름 ide 접속시 Invalid Host header 에러 해결
-		allowedHosts: ['all'],
-		// client: {
-		//   overlay: false
-		// }
-	},
-	transpileDependencies: true,
-});
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: true,
+      }),
+    ],
+  },
+  devServer: {
+    allowedHosts: ['all'],
+  },
+  transpileDependencies: true,
+})
